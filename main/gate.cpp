7d09b51fe5ba38310ea2dd603dfd00cc8a7a57c1
smalljame <smalljame@ecs.vuw.ac.nz>
2023-05-12 10:57:41 +1200

    created and finished quadrant 1
    ran into problems with reciving message but works now

---
+++ main/gate.cpp
@@ -0,0 +1,12 @@
+#include "E101.h"
+
+int main(){
+    int port  = 1024;
+    char server_addr[] = "130.195.3.91";
+    connect_to_server(server_addr, port);
+    char please[] = "Please";
+    send_to_server(please);
+    char password[24];
+    receive_from_server(password);
+    send_to_server(password)
+}
