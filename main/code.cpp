9389ee0f208a3ad79d8b0333830d2b9bacf366b9
smalljame <smalljame@ecs.vuw.ac.nz>
2023-05-15 15:19:48 +1200

    start code for quadrant2 for robot to follow line
    started building bracket to hold carmer to servo

---
+++ main/code.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include "E101.h"
+
+int main(){
+	//Quadrant 1 - Opening the gate 
+    int port  = 1024;
+    char server_addr[] = "130.195.3.91";
+    connect_to_server(server_addr, port);
+    char please[] = "Please";
+    send_to_server(please);
+    char password[24];
+    receive_from_server(password);
+    send_to_server(password);
+    
+    
+    //Quadrant 2 
+    open_screen_stream();
+    
+    // make 1000 runs  
+	for (int countrun = 0; countrun < 1000; countrun++) {
+		take_picture();
+		update_screen();
+		
+		std::vector<int> whitepixels; 	//array to store white pixels 
+    
+		for (int row = 0 ; row == 120 ; row++) {	
+			for (int col = 0; col < 320; col++) {
+	
+		
+				whitepixels.push_back(get_pixel(row, col, 3));
+			}
+		}
+	}
+	close_screen_stream();
+	return 0;
+	
+	
+	
+	
+	//testesttest
+	vector<int> pixels(320); 		//array to store pixels (0s and 1s)
+	//vector<int> indexpixel(320); 	//array to store indexpixel (-160 to 160)
+	
+	int treshold = 40;
+	
+	open_screen_stream();
+	take_picture();
+	for (int col = 0; col < 320; col++) {
+		//set pixel to black if its less than treshold
+		if(get_pixel(120, col, 3) < treshold){
+			pixels[col] = 1; 
+			//indexpixel[col] = 1;
+		}
+		else {
+			pixels[col] = 0; 
+			//indexpixel[col] = 0;
+		}
+		
+		
+	}
+	close_screen_stream();
+	return 0;
+		
+}
+
