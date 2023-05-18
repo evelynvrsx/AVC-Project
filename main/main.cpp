c57ea4a2164f980c932b247a6428baeee66324df
smalljame <smalljame@ecs.vuw.ac.nz>
2023-05-18 21:28:56 +1200

    trying ot fix stuff

--- main/main.cpp
+++ main/main.cpp
@@ -5,11 +5,34 @@
 #include <cstring>
 #include <array>
 #include "E101.h"
-#include "code_hpp/\gate.hpp"
+#include "code_hpp/gate.hpp"
 
+int main(){
+    
+    int error = init(0);
+    std::cout<< "Error count: " << error <<std::endl;
 
-
-using namespace std;
+    //camera functions
+    open_screen_stream();//only needed to be called once
+    //once we test the wiggly lines, we can see how off is the iota from the center 
+    take_picture();
+    update_screen();
+    //end of camera code
+    gate_open();
+    /*check motors
+    set_motors(1, 47);
+    set_motors(2, 47);
+    set_motors(3, 47);w
+    set_motors(4, 47);
+    set_motors(5, 47);
+    hardware_exchange();
+    //PLEASE REMOVE AFTER USE
+    //WARNING!! DO NOT UNPLUG THE MOTOR CABLES AFTER THE HARDWARE IS DONE!!
+    */
+    stoph(); // do not move this anywhere in the system.
+    return 0;   
+}
+//using namespace std;
 
 /*void quad2(){
 	vector<int> pixels(240); 		//array to store pixels (0s and 1s)
@@ -68,28 +91,3 @@ void gate_open(){
    
 
 */
-int main(){
-    
-    int error = init(0);
-    cout<< "Error count: " << error <<endl;
-
-    //camera functions
-    open_screen_stream;//only needed to be called once
-    //once we test the wiggly lines, we can see how off is the iota from the center 
-    take_picture();
-    update_screen();
-    //end of camera code
-    gate_open();
-    /*check motors
-    set_motors(1, 47);
-    set_motors(2, 47);
-    set_motors(3, 47);w
-    set_motors(4, 47);
-    set_motors(5, 47);
-    hardware_exchange();
-    //PLEASE REMOVE AFTER USE
-    //WARNING!! DO NOT UNPLUG THE MOTOR CABLES AFTER THE HARDWARE IS DONE!!
-    */
-    stoph(); // do not move this anywhere in the system.
-    return 0;   
-}
