79a6d46da002d042a1ef3c73eca6d3d47e8b19f4
smalljame <smalljame@ecs.vuw.ac.nz>
2023-05-19 17:06:42 +1200

    doign header files

---
+++ include/motor_contorl.hpp
@@ -0,0 +1,7 @@
+#pragma once
+void move_forward(unsigned char speed);
+void move_back(unsigned char speed);
+void turn(unsigned char pwm ,int time);
+void stop(void);
+void servo_full_down(void);
+void servo_full_up(void);
