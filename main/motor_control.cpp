3c1b9f5af600c13598a556ea2a6e039c3e5b182a
smalljame <smalljame@ecs.vuw.ac.nz>
2023-05-19 22:20:27 +1200

    i defined 48 as MID_POINT

--- main/motor_control.cpp
+++ main/motor_control.cpp
@@ -4,18 +4,19 @@
 #define RIGHT_MOTOR 1 
 #define LEFT_MOTOR  5
 #define SERVO 3
+#define MID_POINT 48
 
 void move_forward(unsigned char speed ){
-    int right = 48 - speed;
-    int left = 48 + speed;
+    int right = MID_POINT - speed;
+    int left = MID_POINT + speed;
     set_motors(RIGHT_MOTOR, right);
     set_motors(LEFT_MOTOR, left);
     hardware_exchange();
 }
 
 void move_back(unsigned char speed){
-    int right = 48 + speed;
-    int left = 48  - speed;
+    int right = MID_POINT + speed;
+    int left = MID_POINT  - speed;
     set_motors(RIGHT_MOTOR, right);
     set_motors(LEFT_MOTOR, left);
     hardware_exchange();
@@ -26,14 +27,14 @@ void turn(unsigned char pwm , int time){
     set_motors(LEFT_MOTOR, pwm);
     hardware_exchange();
     sleep1(time);
-    set_motors(RIGHT_MOTOR, 48);
-    set_motors(LEFT_MOTOR, 48);
+    set_motors(RIGHT_MOTOR, MID_POINT);
+    set_motors(LEFT_MOTOR, MID_POINT);
     hardware_exchange();
 }
 
 void stop(){
-    set_motors(RIGHT_MOTOR, 48);
-    set_motors(LEFT_MOTOR, 48);
+    set_motors(RIGHT_MOTOR, MID_POINT);
+    set_motors(LEFT_MOTOR, MID_POINT);
     hardware_exchange();
 }
 
