5290ac21f829150f1fce8fc23afd3751122bd66b
Evelyn Salim <salimevel@ecs.vuw.ac.nz>
2023-05-24 14:25:32 +1200

    Update quad2.cpp

--- main/quad2.cpp
+++ main/quad2.cpp
@@ -1,70 +1,89 @@
-/*//Quadrant 2 
-    open_screen_stream();
-    
-    // make 1000 runs  
-	for (int countrun = 0; countrun < 1000; countrun++) {
-		take_picture();
-		update_screen();
-		
-		std::vector<int> whitepixels; 	//array to store white pixels 
-    
-		for (int row = 0 ; row == 120 ; row++) {	
-			for (int col = 0; col < 320; col++) {
-	
-		
-				whitepixels.push_back(get_pixel(row, col, 3));
-			}
-		}
-	}
-	close_screen_stream();
-	return 0;
-	
-	*/
 #include <iostream>
-#include <array>
-#include <iterator>
+#include "E101.h"
 #include <numeric>
 #include <cstring>
 #include <array>
-#include "E101.h" 
+#include <algorithm>
+#include "../include/motor_control.hpp"
 
-using namespace std;	
-/* Algorithim for robot to run through quadrant 2*/
-void quad2(){
-	//testesttest
-	int* pixel = new int[320];		//array to store pixels (0s and 1s)
-	//vector<int> indexpixel(320); 	//array to store indexpixel (-160 to 160)
-	
-	int treshold = 40;
-	//int pix[240];
-	
-	
+using namespace std;
 
+int middle_point = 160; 
 
-	for (int col = 0; col < 320; col++) {
-		//set pixel to black if its less than treshold
-		if(get_pixel(120, col, 3) < treshold){
-			pixel[col] = 1; 
-			//indexpixel[col] = 1;
-		}
-		else {
-			pixel[col] = 0; 
-			//indexpixel[col] = 0;
-		}
-		//iota();//begin,end,-120)
+int main(){
+	//Quadrant 1 - Opening the gate 
+    int port  = 1024;
+    char server_addr[] = "130.195.3.91";
+    connect_to_server(server_addr, port);
+    char please[] = "Please";
+    send_to_server(please);
+    char password[24];
+    receive_from_server(password);
+    send_to_server(password);
+    
+    
+    //Quadrant 2 
+    int pixels[320];		//array to store pixels 0s and 1s
+    int indexpixels[320];	//array to store 0s and 1s minus the index
+    int treshold = 60;
+    //double kp = 0.01;
+    //double kd = 0;
+    //double adjustment = kp * error;
+    double middle_point = 120;
+    //double middleIndex = 160;
+    
+    open_screen_stream();
+    take_picture();
+    update_screen();
+    
+    //check the line
+    for (int col = 0; col < 320; col++) {
+		//check the white pixels 
+		int white = get_pixel(middle_point, col, 3);
 		
-		//define movement based on the error
-		
-		if(error > x ){ //x = threshold
-		//turn right
-		}
-		else if(error < 1 && error > -1){
-			//straight
+		if(white < treshold){
+			//black
+			pixels[col] = 1;
 		}
 		else{
-			//adjust left
+			//white
+			pixels[col] = 0;
+		}
+		
+		// middle image (gives a range from -160 to 159
+		iota(indexpixels, indexpixels+320, -160);
+
+		
+		// Subtract middle index from each element of indexpixel
+		//for (unsigned int i = 0; i < 320; i++) {
+			//indexpixels[i]= i - middleIndex;
+		//}
+		
+		// Taking the array of white pixels and multiply each element of it with middle index
+		//int checkpixel[320];
+		//for (unsigned int i = 0; i < 320; i++) {
+			//indexpixels[i] = pixels[i] * indexpixels[i];
+			//checkpixel[i] = pixels[i] * indexpixels[i];
+		//}
+		
+		// Calculate the sum of multiplied elements
+		int error = 0;
+		for (unsigned int i = 0; i < 320; i++) {
+			//error += checkpixel[i];
+			error += indexpixels[i];
+		}
+		std::cout << "Sum: " << error << std::endl;
+		
+		//define movement based on error
+		if (error >= -10 || error <= 10) {
+			move_forward(16);
+		}
+		
 	}
+	
+    close_screen_stream();
+	return 0;
 
-
+}
 		
 }
